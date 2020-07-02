e626e04379eb0a9e8772318a3dd8ec64017b44c2
Shiaoming <xmzhao@buaa.edu.cn>
2020-07-02 16:37:16 +0800

    add FrameByFrameMatcher

---
+++ Matchers/FrameByFrameMatcher.py
@@ -0,0 +1,88 @@
+import numpy as np
+import cv2
+from utils.tools import dict_update
+
+
+class FrameByFrameMatcher(object):
+    default_config = {
+        "KNN": {
+            "HAMMING": True,  # For ORB Binary descriptor, only can use hamming matching
+            "first_N": 300,  # For hamming matching, use first N min matches
+        },
+        "FLANN": {
+            "kdTrees": 5,
+            "searchChecks": 50
+        },
+        "distance_ratio": 0.75
+    }
+
+    def __init__(self, matcher_type="FLANN", config=None):
+        self.config = self.default_config
+        self.config = dict_update(self.config, config)
+        self.matcher_type = matcher_type
+
+        if self.matcher_type == "KNN":
+            if self.config["KNN"]["HAMMING"]:
+                self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            else:
+                self.matcher = cv2.BFMatcher()
+        elif self.matcher_type == "FLANN":
+            # FLANN parameters
+            FLANN_INDEX_KDTREE = 1
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=self.config["FLANN"]["kdTrees"])
+            search_params = dict(checks=self.config["FLANN"]["searchChecks"])  # or pass empty dictionary
+            self.matcher = cv2.FlannBasedMatcher(index_params, search_params)
+        else:
+            raise ValueError(f"Unknown matcher type: {self.matcher_type}")
+
+    def match(self, desc):
+        self.good = []
+        if self.matcher_type == "KNN" and self.config["KNN"]["HAMMING"]:
+            matches = self.matcher.match(desc["ref"], desc["cur"])
+            # Sort them in the order of their distance.
+            matches = sorted(matches, key=lambda x: x.distance)
+            self.good = matches[:self.config["KNN"]["first_N"]]
+        else:
+            matches = self.matcher.knnMatch(desc["ref"], desc["cur"], k=2)
+            # Apply ratio test
+            for m, n in matches:
+                if m.distance < self.config["distance_ratio"] * n.distance:
+                    self.good.append([m])
+        return self.good
+
+    def draw_matches(self, imgs, kpts):
+        if self.matcher_type == "KNN" and self.config["KNN"]["HAMMING"]:
+            img = cv2.drawMatches(imgs["ref"], kpts["ref"],
+                                  imgs["cur"], kpts["cur"],
+                                  good, None,
+                                  flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        else:
+            img = cv2.drawMatchesKnn(imgs["ref"], kpts["ref"],
+                                     imgs["cur"], kpts["cur"],
+                                     good, None,
+                                     flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        return img
+
+
+if __name__ == "__main__":
+    from DataLoader.KITTILoader import KITTILoader
+    from Detectors.HandcraftDetector import HandcraftDetector
+
+    loader = KITTILoader()
+    detector = HandcraftDetector("ORB")
+    matcher = FrameByFrameMatcher("KNN")
+
+    kpts = {}
+    desc = {}
+    imgs = {}
+    for i, img in enumerate(loader):
+        imgs["cur"] = img
+        kpts["cur"], desc["cur"] = detector.detect(img)
+        if i > 1:
+            good = matcher.match(desc)
+            img = matcher.draw_matches(imgs, kpts)
+            cv2.imshow("track", img)
+            if cv2.waitKey() == 27:
+                break
+
+        kpts["ref"], desc["ref"], imgs["ref"] = kpts["cur"], desc["cur"], imgs["cur"]
