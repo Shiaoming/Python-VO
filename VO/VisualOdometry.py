6e4f4c4ec4b64131809f72a6bdbbe2f6106da48d
Shiaoming <xmzhao@buaa.edu.cn>
2020-07-02 15:03:44 +0800

    move PinholeCamera to utils

--- VO/VisualOdometry.py
+++ VO/VisualOdometry.py
@@ -4,20 +4,7 @@ import numpy as np
 import cv2
 
 
-class PinholeCamera:
-    def __init__(self, width, height, fx, fy, cx, cy,
-                 k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
-        self.width = width
-        self.height = height
-        self.fx = fx
-        self.fy = fy
-        self.cx = cx
-        self.cy = cy
-        self.distortion = (abs(k1) > 0.0000001)
-        self.d = [k1, k2, p1, p2, k3]
-
-
-class VisualOdometry:
+class VisualOdometry(object):
     """
     A simple frame by frame visual odometry
     """
---
+++ utils/PinholeCamera.py
@@ -0,0 +1,12 @@
+
+class PinholeCamera(object):
+    def __init__(self, width, height, fx, fy, cx, cy,
+                 k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
+        self.width = width
+        self.height = height
+        self.fx = fx
+        self.fy = fy
+        self.cx = cx
+        self.cy = cy
+        self.distortion = (abs(k1) > 0.0000001)
+        self.d = [k1, k2, p1, p2, k3]
