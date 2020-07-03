b2df28149aab16ce6ab8e6a869aa53cff6782969
Shiaoming <xmzhao@buaa.edu.cn>
2020-07-03 14:30:58 +0800

    add superpoint detector

---
+++ Detectors/SuperPointDetector.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from utils.tools import *
+from Detectors.superpoint.superpoint import SuperPoint
+
+
+class SuperPointDetector(object):
+    default_config = {
+        "descriptor_dim": 256,
+        "nms_radius": 4,
+        "keypoint_threshold": 0.005,
+        "max_keypoints": -1,
+        "remove_borders": 4,
+        "path": Path(__file__).parent / "superpoint/superpoint_v1.pth",
+        "cuda": True
+    }
+
+    def __init__(self, config=None):
+        self.config = self.default_config
+        self.config = dict_update(self.config, config)
+
+        self.device = 'cuda' if torch.cuda.is_available() and self.config["cuda"] else 'cpu'
+
+        self.superpoint = SuperPoint(self.config).to(self.device)
+
+    def __call__(self, image):
+        if image.shape[2] == 3:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        image_tensor = image2tensor(image, self.device)
+        pred = self.superpoint({'image': image_tensor})
+
+        ret_dict = {
+            "torch": pred,
+            "keypoints": pred["keypoints"][0].cpu().detach().numpy(),
+            "scores": pred["scores"][0].cpu().detach().numpy(),
+            "descriptors": pred["descriptors"][0].cpu().detach().numpy().transpose()
+        }
+
+        return ret_dict
+
+
+if __name__ == "__main__":
+    img = cv2.imread("../test_imgs/sequences/00/image_0/000000.png")
+
+    detector = SuperPointDetector()
+    kptdescs = detector(img)
+
+    img = plot_keypoints(img, kptdescs["keypoints"], kptdescs["scores"])
+    cv2.imshow("SuperPoint", img)
+    cv2.waitKey()
