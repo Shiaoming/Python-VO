22db66b67e2defdd058083299cccb5dcf6200460
Shiaoming <xmzhao@buaa.edu.cn>
2020-07-06 11:21:41 +0800

    add a simple SequenceImageLoader

---
+++ DataLoader/SequenceImageLoader.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import glob
+from tqdm import tqdm
+import logging
+
+from utils.tools import dict_update
+
+
+class SequenceImageLoader(object):
+    default_config = {
+        "root_path": "/home/zxm/Pictures/Webcam",
+        "start": 0,
+        "format": "jpg"
+    }
+
+    def __init__(self, config=None):
+        self.config = self.default_config
+        self.config = dict_update(self.config, config)
+        logging.info("Sequence image loader config: ")
+        logging.info(self.config)
+
+        self.img_id = self.config["start"]
+        self.img_N = len(glob.glob(pathname=self.config["root_path"] + "/*." + self.config["format"]))
+
+    def __getitem__(self, item):
+        file_name = self.config["root_path"] + "/" + str(item) + "." + self.config["format"]
+        img = cv2.imread(file_name)
+        return img
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.img_id < self.img_N:
+            img = self.__getitem__(self.img_id)
+
+            self.img_id += 1
+
+            return img
+        raise StopIteration()
+
+    def __len__(self):
+        return self.img_N - self.config["start"]
