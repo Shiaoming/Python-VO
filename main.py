a386e7490eefabba631331e52f71a26e6ce9d64c
Shiaoming <xmzhao@buaa.edu.cn>
2020-12-25 16:06:00 +0800

    test open3d viewer

--- main.py
+++ main.py
@@ -13,11 +13,81 @@ from Detectors import create_detector
 from Matchers import create_matcher
 from VO.VisualOdometry import VisualOdometry, AbosluteScaleComputer
 
+import open3d as o3d
+from multiprocessing import Process, Queue
+
 
 def keypoints_plot(img, vo):
     if img.shape[2] == 1:
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     return plot_keypoints(img, vo.kptdescs["cur"]["keypoints"], vo.kptdescs["cur"]["scores"])
+
+
+class TrajPlotterO3D(object):
+    def __init__(self):
+        # initial
+        self.errors = []
+
+        # multiprocess
+        self.queue = Queue(1)
+        self.process = Process(target=self.loop)
+        self.running = True
+        self.process.start()
+
+    def loop(self):
+        from _queue import Empty
+        # create o3d visualizer
+        vis = o3d.visualization.Visualizer()
+        vis.create_window('Trajectory', width=1600, height=900, top=90, left=160)
+        # add coordinate system
+        coordinate_system = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
+        vis.add_geometry(coordinate_system)
+        # set render option
+        opt = vis.get_render_option()
+        opt.background_color = np.array([0, 0, 0])
+        opt.line_width = 3
+
+        last_gt = np.array([0, 0, 0])
+        last_est = np.array([0, 0, 0])
+        while self.running:
+            try:
+                data_ = self.queue.get(block=False)
+
+                est_line_frag = self.get_line_fragament(data_['est_xyz'], last_est, [1, 0, 0])
+                gt_line_frag = self.get_line_fragament(data_['gt_xyz'], last_gt, [0, 1, 0])
+                last_est = data_['est_xyz']
+                last_gt = data_['gt_xyz']
+
+                # view_control = vis.get_view_control()
+
+                vis.add_geometry(est_line_frag, reset_bounding_box=False)
+                vis.add_geometry(gt_line_frag, reset_bounding_box=False)
+
+                vis.reset_view_point(True)
+            except Empty:
+                pass
+
+            # show
+            vis.poll_events()
+            vis.update_renderer()
+        vis.destroy_window()
+
+    def close(self):
+        self.running = False
+        self.process.join()
+
+    def get_line_fragament(self, cur_xyz, las_xyz, line_color):
+        points = [las_xyz, cur_xyz]
+        lines = [[0, 1]]
+        line_set = o3d.geometry.LineSet(points=o3d.utility.Vector3dVector(points),
+                                        lines=o3d.utility.Vector2iVector(lines), )
+        colors = [line_color for i in range(len(lines))]
+        line_set.colors = o3d.utility.Vector3dVector(colors)
+        return line_set
+
+    def update(self, est_xyz, gt_xyz):
+        self.queue.put({'est_xyz': est_xyz,
+                        'gt_xyz': gt_xyz})
 
 
 class TrajPlotter(object):
@@ -69,7 +139,8 @@ def run(args):
     matcher = create_matcher(config["matcher"])
 
     absscale = AbosluteScaleComputer()
-    traj_plotter = TrajPlotter()
+    # traj_plotter = TrajPlotter()
+    traj_plotter = TrajPlotterO3D()
 
     # log
     fname = args.config.split('/')[-1].split('.')[0]
@@ -85,14 +156,15 @@ def run(args):
 
         # === drawer ==================================
         img1 = keypoints_plot(img, vo)
-        img2 = traj_plotter.update(t, gt_pose[:, 3])
+        traj_plotter.update(t, gt_pose[:, 3])
 
         cv2.imshow("keypoints", img1)
-        cv2.imshow("trajectory", img2)
-        if cv2.waitKey(10) == 27:
+        if cv2.waitKey() == 27:
             break
 
-    cv2.imwrite("results/" + fname + '.png', img2)
+    cv2.waitKey()
+    traj_plotter.close()
+    # cv2.imwrite("results/" + fname + '.png', img2)
 
 
 if __name__ == "__main__":
